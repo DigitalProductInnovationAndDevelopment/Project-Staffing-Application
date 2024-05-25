e10ee962d6ed12ddafe9b81dcbc3ca3810bfe9d0
„handes-yilmaz“ <hande.ylmaz@tum.de>
2024-05-25 21:50:19 +0300

    Login page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Login from './pages/auth/Login.js'; 
+import './style.scss';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#e2e8f0',
+    },
+    secondary: {
+      main: '#4fd1c5',
+    },
+  },
+  components: {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          '& label.Mui-focused': {
+            color: '#4fd1c5',
+          },
+          '& .MuiInput-underline:after': {
+            borderBottomColor: '#4fd1c5',
+          },
+          '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+              borderColor: '#e2e8f0',
+            },
+            '&:hover fieldset': {
+              borderColor: '#4fd1c5',
+            },
+            '&.Mui-focused fieldset': {
+              borderColor: '#4fd1c5',
+            },
+          },
+        },
+      },
+    },
+  },
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          GREAT STAFF
-        </p>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}> 
+      <div className="App">
+        <Login /> 
+      </div>
+    </ThemeProvider> 
   );
 }
 
