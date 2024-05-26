96dd1815760c99d5065ecbe350b4748c92e719c9
martin-stierlen <martin.stierlen@tum.de>
2024-05-26 10:43:29 +0200

    set-up backend folder structure

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+
+
+/* CONFIGURATIONS */
+TODO
+
+/* MIDDLEWARE (later) */
+
+/* ROUTES */
+TODO
+
+/* UNKNOWN ROUTES */
+TODO
+
+/* GLOBAL ERROR HANDLING */
+TODO
+
+/* MONGOOSE SETUP */
+TODO
+
