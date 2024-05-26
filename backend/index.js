f78f725dbdde3799f7efdc6fd08f2674bd92cf17
martin-stierlen <martin.stierlen@tum.de>
2024-05-26 18:26:59 +0200

    setup basic server (hello world!) and established connection to mongoDB cluster

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,45 @@
 import express from "express";
-import mongoose from "mongoose";
-
+import bodyParser from "body-parser";
+import mongoose, { mongo } from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
 
 /* CONFIGURATIONS */
-TODO
-
-/* MIDDLEWARE (later) */
-
-/* ROUTES */
-TODO
-
-/* UNKNOWN ROUTES */
-TODO
-
-/* GLOBAL ERROR HANDLING */
-TODO
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, 'public/assets'))); // set directory of where to store our assets (i.e. images) (in this case locally)
 
 /* MONGOOSE SETUP */
-TODO
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
 
+
+app.get('/', (req, res) => {
+    res.send('Hello World! This is the GREAT STAFF server!');
+});
+
+/* MIDDLEWARE */
+/* ROUTES */
+/* UNKNOWN ROUTES */
+/* GLOBAL ERROR HANDLING */
