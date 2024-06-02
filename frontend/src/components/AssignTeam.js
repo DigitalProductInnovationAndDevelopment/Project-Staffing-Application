7c5aa949ce992075d6da0d4abeb767010b62eb86
Hande <hande.yilmaz@checktur.io>
2024-06-02 22:05:41 +0300

    project edit dialog ui layout

---
+++ frontend/src/components/AssignTeam.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const AssignTeam = () => {
+  return (
+    <Box>
+      <Typography variant="body1">This is the Assign Team tab content.</Typography>
+    </Box>
+  );
+};
+
+export default AssignTeam;
---
+++ frontend/src/components/Overview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const Overview = () => {
+  return (
+    <Box>
+      <Typography variant="body1">This is the Overview tab content.</Typography>
+    </Box>
+  );
+};
+
+export default Overview;
