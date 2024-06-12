ce4fa13c05cb3d58241dd01fb7aece65e78ef16b
martin-stierlen <martin.stierlen@tum.de>
2024-06-12 12:26:58 +0200

    implement 4 main routes (authentication, user, project, profile)

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -1,10 +1,19 @@
 import express from "express";
+import { createNewProfileController, getAllProfilesByProjectIdController, getProfileByIdController, updateProfileController, deleteProfileController } from "../controllers/profile.js";
 
 const router = express.Router();
 
 // CREATE
+router.post("/:projectId", createNewProfileController);
+
 // READ
+router.get("/:projectId/profiles", getAllProfilesByProjectIdController);
+router.get("/:projectId/:profileId", getProfileByIdController);
+
 // UPDATE
+router.put("/:projectId/:profileId", updateProfileController);
+
 // DELETE
+router.delete("/:projectId/:profileId", deleteProfileController);
 
 export default router;
