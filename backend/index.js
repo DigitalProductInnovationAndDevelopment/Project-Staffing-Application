cbcf3182a68975eb46cbc93724a61cdecb7fbbd4
martin-stierlen <martin.stierlen@tum.de>
2024-06-13 12:57:37 +0200

    update mongoose setup in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
@@ -12,7 +12,6 @@ import authRoutes from "./routes/authentication.js";
 import userRoutes from "./routes/user.js";
 import projectRoutes from "./routes/project.js";
 import profileRoutes from "./routes/profile.js";
-
 
 
 /* CONFIGURATIONS */
@@ -29,30 +28,17 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets'))); // set directory of where to store our assets (i.e. images) (in this case locally)
 
-/* MONGOOSE SETUP */
-const PORT = process.env.PORT || 6001;
-mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-  })
-  .catch((error) => console.log(`${error} did not connect`));
-
-app.get('/', (req, res) => {
-    res.send('Hello World! This is the GREAT STAFF server!');
-});
-
 /* MIDDLEWARE */
-
 
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
 app.use("/project", projectRoutes);
 app.use("/profile", profileRoutes);
+//home route (remove later)
+app.get('/', (req, res) => {
+  res.send('Hello World! This is the GREAT STAFF server!');
+});
 
 /* UNKNOWN ROUTES */
 app.all('*', (req, res, next) => {
@@ -61,5 +47,21 @@ app.all('*', (req, res, next) => {
   next(err);
 });
 
-
 /* GLOBAL ERROR HANDLING */
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async() => {
+    //list current database collections
+    const db = mongoose.connection.client.db();
+    const collections = await db.listCollections().toArray();
+    const collectionNames = collections.map(collection => collection.name);
+    console.log('current database collections (tables):', collectionNames);
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
