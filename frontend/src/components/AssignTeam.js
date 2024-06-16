8a1c083b822c30b20201d29559a017b737b07771
Hande <hande.yilmaz@checktur.io>
2024-06-16 15:31:43 +0200

    avatar added to assig team employees

--- frontend/src/components/AssignTeam.js
+++ frontend/src/components/AssignTeam.js
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import { Box, Typography, Tabs, Tab, Slider, Avatar, Button } from '@mui/material';
 import addIcon from './../assets/images/add-icon.svg';
 import removeIcon from './../assets/images/remove-icon.svg';
+import AvatarEx4 from './../assets/images/icons/avatar_ex4.svg';
+import AvatarGreen from './../assets/images/icons/green_avatar.svg';
+import AvatarBlue from './../assets/images/icons/blue_avatar.svg';
+import AvatarDB from './../assets/images/icons/dblue_avatar.svg';
+import AvatarPurple from './../assets/images/icons/purple_avatar.svg';
 
 const skillsets = {
   'PROJECT LEAD': [
@@ -36,19 +41,9 @@ const skillsets = {
 
 const employees = [
   {
-    name: 'Andrej Karpathy',
-    email: 'karpathy@itestra.com',
-    skills: [
-      { skill: 'Technology', points: '5/10' },
-      { skill: 'Solution Engineering', points: '4/10' },
-      { skill: 'Self-Management', points: '6/10' },
-      { skill: 'Communication Skills', points: '6/10' },
-      { skill: 'Employee Leadership', points: '8/10' },
-    ],
-  },
-  {
     name: 'Peter Drucker',
     email: 'drucker@itestra.com',
+    avatar: AvatarEx4,
     skills: [
       { skill: 'Technology', points: '3/10' },
       { skill: 'Solution Engineering', points: '1/10' },
@@ -58,8 +53,57 @@ const employees = [
     ],
   },
   {
+    name: 'Andrej Karpathy',
+    email: 'karpathy@itestra.com',
+    avatar: AvatarDB,
+    skills: [
+      { skill: 'Technology', points: '8/10' },
+      { skill: 'Solution Engineering', points: '6/10' },
+      { skill: 'Self-Management', points: '7/10' },
+      { skill: 'Communication Skills', points: '2/10' },
+      { skill: 'Employee Leadership', points: '5/10' },
+    ],
+  },
+  {
+    name: 'Kales Urany',
+    email: 'kurany@itestra.com',
+    avatar: AvatarPurple,
+    skills: [
+      { skill: 'Technology', points: '5/10' },
+      { skill: 'Solution Engineering', points: '4/10' },
+      { skill: 'Self-Management', points: '6/10' },
+      { skill: 'Communication Skills', points: '6/10' },
+      { skill: 'Employee Leadership', points: '8/10' },
+    ],
+  },
+  {
+    name: 'Umay Cental',
+    email: 'ucental@itestra.com',
+    avatar: AvatarBlue,
+    skills: [
+      { skill: 'Technology', points: '1/10' },
+      { skill: 'Solution Engineering', points: '10/10' },
+      { skill: 'Self-Management', points: '2/10' },
+      { skill: 'Communication Skills', points: '5/10' },
+      { skill: 'Employee Leadership', points: '6/10' },
+    ],
+  },
+  {
+    name: 'Halsey Allison',
+    email: 'halsey@itestra.com',
+    avatar: AvatarGreen,
+    skills: [
+      { skill: 'Technology', points: '6/10' },
+      { skill: 'Solution Engineering', points: '1/10' },
+      { skill: 'Self-Management', points: '6/10' },
+      { skill: 'Communication Skills', points: '9/10' },
+      { skill: 'Employee Leadership', points: '4/10' },
+    ],
+  },
+  {
     name: 'Andrew Ng',
     email: 'andrew@itestra.com',
+    avatar: AvatarGreen,
     skills: [
       { skill: 'Frontend Development', points: '8/10' },
       { skill: 'Backend Development', points: '5/10' },
@@ -71,6 +115,7 @@ const employees = [
   {
     name: 'Paul Paulsen',
     email: 'paulsen@itestra.com',
+    avatar: AvatarDB,
     skills: [
       { skill: 'Frontend Development', points: '8/10' },
       { skill: 'Backend Development', points: '7/10' },
@@ -348,7 +393,7 @@ const AssignTeam = () => {
         </Typography>
         {assigned[tabLabels[activeTab]].map((employee) => (
           <Box key={employee.email} sx={{ display: 'flex', alignItems: 'center', marginBottom: 1, borderBottom: '1px solid #E2E8F0', paddingBottom: 1 }}>
-            <Avatar sx={{ marginRight: 2 }}>{employee.name[0]}</Avatar>
+            <Avatar src={employee.avatar} sx={{ marginRight: 2, borderRadius: '15px', overflow: 'hidden'}}>{employee.name[0]}</Avatar>
             <Box  sx={{ flex: 0, display: 'flex', flexDirection: 'column', justifyContent: 'center', mr: '30px'}}>
               <Typography
                 sx={{fontFamily: 'Helvetica, sans-serif', fontSize: '14px', lineHeight: '140%', letterSpacing: '0', fontWeight: 'bold', color: '#2D3748', marginBlock: '0'}}
@@ -425,7 +470,7 @@ const AssignTeam = () => {
         </Typography>
         {getFilteredEmployees().map((employee) => (
           <Box key={employee.email} sx={{ display: 'flex', alignItems: 'center', marginBottom: 1, borderBottom: '1px solid #E2E8F0', paddingBottom: 1 }}>
-            <Avatar sx={{ marginRight: 2 }}>{employee.name[0]}</Avatar>
+            <Avatar src={employee.avatar} sx={{ marginRight: 2, borderRadius: '15px', overflow: 'hidden'}}>{employee.name[0]}</Avatar>
             <Box sx={{ flex: 0, display: 'flex', flexDirection: 'column', justifyContent: 'center', mr: '30px' }}>
               <Typography
                sx={{fontFamily: 'Helvetica, sans-serif', fontSize: '14px', lineHeight: '140%', letterSpacing: '0', fontWeight: 'bold', color: '#2D3748', marginBlock: '0'}}
