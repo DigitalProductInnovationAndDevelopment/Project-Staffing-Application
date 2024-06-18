c3c31d226b5ceab10cb1d7980f2aeea727aabf4f
martin-stierlen <martin.stierlen@tum.de>
2024-06-18 13:32:11 +0200

    renamed "authentication" files to "auth" files

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerController, loginController, logoutController } from "../controllers/authentication.js";
+import { registerController, loginController, logoutController } from "../controllers/auth.js";
 
 const router = express.Router();
 
