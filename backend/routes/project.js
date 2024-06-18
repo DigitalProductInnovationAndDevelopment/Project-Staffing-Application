57f54cb859c44116568528477c3251be0e841c84
martin-stierlen <martin.stierlen@tum.de>
2024-06-18 13:32:52 +0200

    added "profile" routes to "project" routes (since profiles are dependent on project id)

--- backend/routes/project.js
+++ backend/routes/project.js
@@ -1,20 +1,32 @@
 import express from "express";
 import { createNewProjectController, getAllProjectsController, getProjectByIdController, getProjectAssignmentByProjectIdController, updateProjectController, updateProjectAssignmentController } from "../controllers/project.js";
+import { createNewProfileController, getAllProfilesByProjectIdController, getProfileByIdController, updateProfileController, deleteProfileController } from "../controllers/profile.js";
 
 const router = express.Router();
 
 // CREATE
 router.post("/", createNewProjectController);
 
+router.post("/:projectId", createNewProfileController);
+
+
 // READ
 router.get("/", getAllProjectsController);
 router.get("/:projectId", getProjectByIdController);
 router.get("/:projectId/assign", getProjectAssignmentByProjectIdController);
 
+router.get("/:projectId/profiles", getAllProfilesByProjectIdController);
+router.get("/:projectId/:profileId", getProfileByIdController);
+
 // UPDATE
 router.put("/:projectId", updateProjectController);
 router.put("/:projectId/assign", updateProjectAssignmentController);
 
+router.put("/:projectId/:profileId", updateProfileController);
+
+
 // DELETE
+router.delete("/:projectId/:profileId", deleteProfileController);
+
 
 export default router;
