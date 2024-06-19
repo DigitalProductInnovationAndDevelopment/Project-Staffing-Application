49166c9acbfa25739b6e79145141b08eaf36ec49
martin-stierlen <martin.stierlen@tum.de>
2024-06-19 15:16:19 +0200

    correct skill model (remove array)

--- backend/models/Skill.js
+++ backend/models/Skill.js
@@ -1,21 +1,22 @@
-import mongoose from "mongoose";
-import SkillCategory from "./enums/SkillCategory.js"
-
+import mongoose from 'mongoose';
+import SkillCategory from './enums/SkillCategory.js';
 
 const SkillSchema = new mongoose.Schema(
-    {
-      skillPoints: { type: Number, required: true },
-      maxSkillPoints: { type: Number, required: true },
-      profileId: { type: mongoose.Schema.Types.ObjectId, ref: 'Profile', required: true },
-      skillCategories: [{
-        type: String,
-        enum: Object.values(SkillCategory),
-      }],
+  {
+    skillPoints: { type: Number, required: true },
+    maxSkillPoints: { type: Number, required: true },
+    profileId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Profile',
+      required: true,
     },
-    {timestamps: true}
+    skillCategory: {
+      type: String,
+      enum: Object.values(SkillCategory),
+    },
+  },
+  { timestamps: true }
 );
 
-const Skill = mongoose.model("Skill", SkillSchema);
+const Skill = mongoose.model('Skill', SkillSchema);
 export default Skill;
-
-
