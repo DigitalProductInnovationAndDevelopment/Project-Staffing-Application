75cff79c216be81d84ba0566eca97e182f0ea932
Hande <hande.yilmaz@checktur.io>
2024-06-28 01:57:37 +0200

    data fetched for employees

--- frontend/src/pages/Projects.js
+++ frontend/src/pages/Projects.js
@@ -93,7 +93,7 @@ function ProjectOverview() {
               }}
               >Current Projects</Typography>
 
-            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, maxHeight: '80vh', overflowY: 'auto' }}>
               <Box sx={{ display: 'flex', color: '#A0AEC0', fontFamily: 'Helvetica, sans-serif', fontWeight: 'bold' }}>
                 <Box sx={{ width: '25%', paddingLeft: 1 }}>
                   <Typography className="table-title">PROJECT TITLE</Typography>
--- frontend/src/state/store.js
+++ frontend/src/state/store.js
@@ -1,11 +1,13 @@
 // src/state/store.js
 import { configureStore } from '@reduxjs/toolkit';
 import { projectApi } from './api/projectApi';
+import { employeeApi } from './api/employeeApi';
 
 export const store = configureStore({
   reducer: {
     [projectApi.reducerPath]: projectApi.reducer,
+    [employeeApi.reducerPath]: employeeApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(projectApi.middleware),
+    getDefaultMiddleware().concat(projectApi.middleware, employeeApi.middleware),
 });
