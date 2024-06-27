c1ac954cff5f065dd8eb7f2dfb7b48ade72b2f77
Hande <hande.yilmaz@checktur.io>
2024-06-28 01:14:27 +0200

    project data fetched and added

--- frontend/src/pages/Projects.js
+++ frontend/src/pages/Projects.js
@@ -1,82 +1,11 @@
 import React, { useState } from "react";
 import { Box, Typography, Button, Divider, Avatar, AvatarGroup, IconButton, LinearProgress } from "@mui/material";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import spotifyIcon from './../assets/images/icons/spotify-2 1.svg';
-import slackIcon from './../assets/images/icons/slack-new-logo 1.svg';
-import JiraIcon from './../assets/images/icons/jira-3 1.svg';
-import AdobeIcon from './../assets/images/icons/Adobe_XD_CC_icon 1.svg';
-import AltassianIcon from './../assets/images/icons/Icon.svg';
 import { projectApi } from "../state/api/projectApi.js";
 import EditProject from './../components/EditProject';
 
 function ProjectOverview() {
   const { data: projectData, isError, isLoading, isSuccess } = projectApi.endpoints.getAllProjects.useQuery();
-
-  const projects = [
-    {
-      icon: AdobeIcon,
-      title: "Mobile App Performance",
-      company: "Itestra",
-      avatars: [
-        "/mnt/data/A_professional-looking_avatar_of_a_person_with_a_f.png",
-        "/mnt/data/A_professional-looking_avatar_of_a_person_with_a_f.png",
-        "/mnt/data/A_professional-looking_avatar_of_a_person_with_a_f.png",
-        "/mnt/data/A_professional-looking_avatar_of_a_person_with_a_f.png",
-      ],
-      fte: "5 FTE",
-      capacity: "80%",
-      status: "Not Started",
-      priority: "high",
-      daysLeft: "5 Days left",
-    },
-    {
-      icon: slackIcon,
-      title: "Add Progress Track",
-      company: "Company B",
-      avatars: [
-        "/mnt/data/A_professional-looking_avatar_of_a_person_with_a_f.png",
-        "/mnt/data/A_professional-looking_avatar_of_a_person_with_a_f.png",
-      ],
-      fte: "9 FTE",
-      capacity: "23%",
-      status: "Not Started",
-      priority: "normal",
-      daysLeft: "14 Days left",
-    },
-    {
-      icon: JiraIcon,
-      title: "Fix Platform Errors",
-      company: "Company C",
-      avatars: [],
-      fte: "4 FTE",
-      capacity: "0%",
-      status: "Not Started",
-      priority: "normal",
-      daysLeft: "20 Days left",
-    },
-    {
-      icon: spotifyIcon,
-      title: "Launch our Mobile App",
-      company: "Company D",
-      avatars: [],
-      fte: "11 FTE",
-      capacity: "0%",
-      status: "Not Started",
-      priority: "normal",
-      daysLeft: "35 Days left",
-    },
-    {
-      icon: AltassianIcon,
-      title: "Add the New Pricing Page",
-      company: "Company E",
-      avatars: [],
-      fte: "2 FTE",
-      capacity: "0%",
-      status: "Not Started",
-      priority: "normal",
-      daysLeft: "120 Days left",
-    },
-  ];
 
   const [open, setOpen] = useState(false);
   const [selectedProject, setSelectedProject] = useState(null);
@@ -96,8 +25,18 @@ function ProjectOverview() {
     setSelectedProject(null);
   };
 
+  const calculateDaysLeft = (kickoffDate) => {
+    const currentDate = new Date();
+    const startDate = new Date(kickoffDate);
+    const diffTime = startDate - currentDate;
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+    if(diffDays < 0) return 'Started';
+    else return `${diffDays} Days left`;
+  };
+
+
   if (isError) {
-    console.log("isError");
     return (
       <>
         <p>isError</p>
@@ -106,7 +45,6 @@ function ProjectOverview() {
   }
 
   if (isLoading) {
-    console.log("isLoading");
     return (
       <>
         <p>isLoading</p>
@@ -115,7 +53,6 @@ function ProjectOverview() {
   }
 
   if (isSuccess) {
-    console.log("isSuccess");
     return (
       <Box sx={{ display: "flex", height: "100vh", backgroundColor: "#F5F7FA", boxShadow: "none" }}>
         <Box sx={{ flexGrow: 1, p: 3 }}>
@@ -156,20 +93,6 @@ function ProjectOverview() {
               }}
               >Current Projects</Typography>
 
-              {/* DISPLAY FETCHED DATA FROM BACKEND */}
-              {/* 
-        
-              <Box sx={{ p: 2 }}>
-                <Typography variant="h4" gutterBottom>Project Names</Typography>
-                <ul>
-                  {projectData.projects.map((project) => (
-                  <li key={project._id}>{project.projectName}</li>
-                  ))}
-                </ul>
-              </Box>
-        
-              */}
-
             <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
               <Box sx={{ display: 'flex', color: '#A0AEC0', fontFamily: 'Helvetica, sans-serif', fontWeight: 'bold' }}>
                 <Box sx={{ width: '25%', paddingLeft: 1 }}>
@@ -197,14 +120,14 @@ function ProjectOverview() {
                   <Typography className="table-title">EDIT</Typography>
                 </Box>
               </Box>
-              {projects.map((project, index) => (
+              {projectData.projects.map((project, index) => (
                 <React.Fragment key={index}>
                   <Divider />
                   <Box sx={{ display: 'flex', alignItems: 'center', paddingY: 1 }}>
                     <Box sx={{ display: 'flex', alignItems: 'center', width: '25%', paddingLeft: 1 }}>
-                      <Avatar src={project.icon} sx={{ "& .MuiAvatar-img": { width: 24, height: 24, }, 
+                      <Avatar src={project.icon || ''} sx={{ "& .MuiAvatar-img": { width: 24, height: 24 }, 
                         "& .MuiAvatar-root": { borderRadius: '15px', overflow: 'hidden' } }} />
-                      <Typography variant="body2">{project.title}</Typography>
+                      <Typography sx={{ ml: '8px' }} variant="body2">{project.projectName}</Typography>
                     </Box>
                     <Box sx={{ width: '15%' }}>
                       <AvatarGroup max={4} sx={{ 
@@ -212,18 +135,18 @@ function ProjectOverview() {
                         flexDirection: 'row', 
                         ml: '14px'
                       }}>
-                        {project.avatars.map((avatar, index) => (
+                        {(project.avatars || []).map((avatar, index) => (
                           <Avatar key={index} src={avatar} />
                         ))}
                       </AvatarGroup>
                     </Box>
                     <Box sx={{ width: '10%' }}>
-                      <Typography variant="body2">{project.fte}</Typography>
+                      <Typography variant="body2">{project.fte || 'N/A'}</Typography>
                     </Box>
                     <Box sx={{ width: '10%' }}>
-                      <Typography variant="body2">{project.status}</Typography>
+                      <Typography variant="body2">{project.status || 'N/A'}</Typography>
                     </Box>
-                    <Box sx={{  display: 'flex', flexDirection: 'column', width: '20%' }}>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', width: '20%' }}>
                       <Typography sx={{ mr: 1, color: {
                         color:
                         project.capacity >= '80%' ? "#4FD1C5" : // Türkis bei 80% oder höher
@@ -232,10 +155,10 @@ function ProjectOverview() {
                       },
                         fontFamily: 'Helvetica, sans-serif',
                         fontWeight: 'bold',
-                      }}>{project.capacity}</Typography>
+                      }}>{project.capacity || '0%'}</Typography>
                       <LinearProgress
                         variant="determinate"
-                        value={parseInt(project.capacity)}
+                        value={parseInt(project.capacity) || 0}
                         sx={{
                           width: "60%",
                           height: '6px',
@@ -250,10 +173,10 @@ function ProjectOverview() {
                       />
                     </Box>
                     <Box sx={{ width: '10%' }}>
-                      <Typography variant="body2">{project.priority}</Typography>
+                      <Typography variant="body2">{project.priority.toLowerCase()}</Typography>
                     </Box>
                     <Box sx={{ width: '10%' }}>
-                      <Typography variant="body2">{project.daysLeft}</Typography>
+                      <Typography variant="body2">{calculateDaysLeft(project.kickoffDate)}</Typography>
                     </Box>
                     <Box sx={{ width: '10%' }}>
                       <IconButton size="small" onClick={() => handleOpenEditDialog(project)}>
@@ -267,7 +190,7 @@ function ProjectOverview() {
           </Box>
         </Box>
         {selectedProject && (
-          <EditProject open={open} onClose={handleCloseEditDialog} project={{ name: selectedProject.title, company: selectedProject.company, image: selectedProject.icon }} />
+          <EditProject open={open} onClose={handleCloseEditDialog} project={{ name: selectedProject.projectName, company: selectedProject.company, image: selectedProject.icon }} />
         )}
       </Box>
     );
